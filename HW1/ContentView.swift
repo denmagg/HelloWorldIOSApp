1b3699482efaabaacf722cbfbd408d2d88b52908
Denis <denmagg.work@gmail.com>
2021-01-06 05:59:12 +0300

    Initial Commit

---
+++ HW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW1
+//
+//  Created by Denis Medvedev on 06/01/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
